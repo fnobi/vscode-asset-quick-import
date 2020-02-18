a68f61fd9390139b454a841e28631edb5eb59820
nobii <fujisawa-shin@kayac.com>
2020-02-19 01:45:16 +0900

    機能実装おわり

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,63 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as find from "find";
+import * as path from "path";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+// TODO: ほんとはこのへんカスタマイズしたいよね
+const SRC_PATH = "src";
+const SRC_PREFIX = "~/";
+const ASSET_PREFIX = "ASSETS_";
+const PATTERN = /\.(png|jpe?g|gif|mp3|wav)$/;
+
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "assetquickimport" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+		const editor = vscode.window.activeTextEditor;
+		if (!editor) {
+			vscode.window.showErrorMessage("Error: active editor is not found.");
+			return;
+		}
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+		const folders = vscode.workspace.workspaceFolders;
+		if (!folders) {
+			return;
+		}
+		const [root] = [...folders.values()];
+		const srcRoot = path.join(root.uri.path, SRC_PATH);
+		find.file(PATTERN, srcRoot, (files) => {
+			const items = files.map((p) => {
+				const baseName = path.basename(p);
+				const subPath = path.relative(srcRoot, p);
+				const dirName = path.dirname(subPath);
+				return {
+					label: baseName,
+					detail: dirName,
+					baseName,
+					subPath
+				};
+			});
+
+			if (!items.length) {
+				vscode.window.showErrorMessage("Error: assets is not found.");
+				return;
+			}
+
+			vscode.window.showQuickPick(items, { matchOnDetail: true }).then((res) => {
+				if (!res) {
+					return;
+				}
+				const { subPath, baseName } = res;
+				const varName = baseName.replace(PATTERN, "").replace(/-/g, "_").toUpperCase();
+				const tmpAssetName = `${ASSET_PREFIX}${varName}`;
+				const importStatement = `import ${tmpAssetName} from "${SRC_PREFIX}${subPath}";`;
+
+				editor.edit((e) => {
+					e.insert(editor.selection.start, importStatement);
+				});
+			});
+		});
 	});
 
 	context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
